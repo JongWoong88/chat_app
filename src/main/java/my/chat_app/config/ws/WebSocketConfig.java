79cc6af5790d865843e1d4f668fba3bb5bea03db
JongWoong <fiveeyes11@gmail.com>
2025-04-05 01:01:31 +0900

    최소한의 디자인

--- src/main/java/my/chat_app/config/ws/WebSocketConfig.java
+++ src/main/java/my/chat_app/config/ws/WebSocketConfig.java
@@ -1,10 +1,10 @@
 package my.chat_app.config.ws;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
